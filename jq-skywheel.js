c52c4b3c0cb7450b60e2302266bb55b562e62fd2
guoxc <qazwsxedc121@gmail.com>
2013-11-17 02:28:28 +0800

    customize height and width

--- jq-skywheel.js
+++ jq-skywheel.js
@@ -1,13 +1,22 @@
 (function ($) {
     'use strict';
-    jQuery.fn.skywheel =  function () {
+    jQuery.fn.skywheel =  function (options) {
+        var opt = $.extend({}, $.fn.skywheel.defaults, options);
+        var sheet = (function() {
+            var style = document.createElement("style");
+            document.head.appendChild(style);
+            return style.sheet;
+        })();
+        sheet.insertRule(".jq_skywheel li{height:"+opt.sizey+";width:"+opt.sizex+";line-height:"+opt.sizey+";}",sheet.cssRules.length);
         this.addClass("jq_skywheel");
+        this.children().each(function (index, el) {
+            $(el).contents().wrap("<span class='inner'></span>");
+        });
         var lilist = this.children(),
             lilen = lilist.length,
             i,
-            k,
             that = this,
-            adjust = function adjust (index) {
+            adjust = function adjust(index) {
                 var i = 0,
                     k = 0,
                     j = 1;
@@ -29,15 +38,15 @@
                     j += 1;
                 }
             },
-            keyhandler = function keyhandler (event) {
+            keyhandler = function keyhandler(event) {
                 var keyCode = event.keyCode,
                     tomove = that.chosen;
-                if (keyCode == 38) {
-                    tomove = tomove <= 0 ? lilen - 1: tomove - 1;
+                if (keyCode === 38) {
+                    tomove = tomove <= 0 ? lilen - 1 : tomove - 1;
                     adjust(tomove);
                     that.chosen = tomove;
-                }else if (keyCode == 40) {
-                    tomove = tomove >= (lilen - 1) ? 0: tomove + 1;
+                } else if (keyCode === 40) {
+                    tomove = tomove >= (lilen - 1) ? 0 : tomove + 1;
                     adjust(tomove);
                     that.chosen = tomove;
                 }
@@ -64,5 +73,10 @@
             $(el).on("mouseenter", helperin);
             $(el).on("mouseleave", helperout);
         });
+    };
+    jQuery.fn.skywheel.defaults = {
+        type: "normal",
+        sizex: "100px",
+        sizey: "40px",
     };
 }(jQuery));
